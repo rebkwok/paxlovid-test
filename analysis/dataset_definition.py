a3e90aacf8e18423a5ae05a5a893770bedd86e1d
Becky Smith <becky.smith@thedatalab.org>
2023-10-11 14:02:37 +0100

    Simplified dataset definition

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,4 +1,4 @@
-from ehrql import create_dataset, codelist_from_csv
+from ehrql import create_dataset, codelist_from_csv, minimum_of
 from ehrql.tables.beta.core import medications
 from ehrql.tables.beta.tpp import practice_registrations
 
@@ -16,15 +16,21 @@ paxlovid_GP_codes = codelist_from_csv("codelists/user-bangzheng-paxlovid-dmd.csv
 dataset = create_dataset()
 
 # Find first prescription in July 2023
-paxlovid_prescriptions = medications.where(
-  medications.dmd_code.is_in(paxlovid_GP_codes)
-).where(
-  medications.date.is_on_or_between(args.start, args.end)
-).sort_by(medications.date).first_for_patient()
+
+def paxlovid_prescriptions_between(start, end):
+    return (
+        medications.where(medications.dmd_code.is_in(paxlovid_GP_codes))
+        .where(medications.date.is_on_or_between(start, end))
+        .sort_by(medications.date)
+        .count_for_patient()
+    )
+
+dataset.may = paxlovid_prescriptions_between("2023-05-01", "2023-05-31")
+dataset.june = paxlovid_prescriptions_between("2023-06-01", "2023-06-30")
+dataset.july = paxlovid_prescriptions_between("2023-07-01", "2023-07-31")
+dataset.august = paxlovid_prescriptions_between("2023-08-01", "2023-08-31")
 
 # Define population as patients registered with a practice on the date of their prescription
-registered = practice_registrations.for_patient_on(paxlovid_prescriptions.date)
-dataset.define_population(registered.exists_for_patient())
-
-# Output date of prescription
-dataset.date_treated = paxlovid_prescriptions.date
+registered_at_start = practice_registrations.for_patient_on("2023-01-05").exists_for_patient()
+registered_at_end = practice_registrations.for_patient_on("2023-08-30").exists_for_patient()
+dataset.define_population(registered_at_start & registered_at_end)
