848350ad7b00b8ab0f47cc4d48fee62ae617f43e
Becky Smith <becky.smith@thedatalab.org>
2023-10-11 14:31:25 +0100

    Sum prescriptions and patients separately

--- analysis/count.py
+++ analysis/count.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
-df = pd.read_feather("output/dataset.arrow")
-df.drop("patient_id", axis=1, inplace=True)
-total_by_month = df.sum()
-total_by_month.to_csv("output/count.csv", index=True, index_label="Month", header=["Total Prescriptions"])
+# Read df with counts per patient per month
+totals_df = pd.read_feather("output/dataset.arrow")
+totals_df.drop("patient_id", axis=1, inplace=True)
+
+# New df with 1/0 to indicate patient has at least one prescription
+exists_df = totals_df.copy()
+exists_df[exists_df > 0] = 1
+
+summed_by_month = pd.concat([totals_df.sum(), exists_df.sum()], axis=1)
+
+summed_by_month.to_csv(
+    "output/count.csv", 
+    index=True, 
+    index_label="Month", 
+    header=["Total Prescriptions", "Total Patients"]
+)
+
